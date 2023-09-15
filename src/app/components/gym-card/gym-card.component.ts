4b55015c439610a9a3370b2d735dd87bd01922c7
JonathanAguirre <jonathan621924@gmail.com>
2023-09-15 14:20:09 -0300

    agregando z-index a mi nabvar

--- src/app/components/gym-card/gym-card.component.ts
+++ src/app/components/gym-card/gym-card.component.ts
@@ -6,14 +6,11 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./gym-card.component.scss']
 })
 export class GymCardComponent {
- @Input() title: string = "";
- @Input() data: string = "";
  @Input() button: string = "";
  @Input() image: string = "";
  @Output() ejercicios = new EventEmitter<string>();
  
  emitirEjercicios() {
-  console.log(this.title)
-  this.ejercicios.emit(this.title);
+  this.ejercicios.emit(this.button);
  }
 }
